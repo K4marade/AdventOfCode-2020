28eda2e7a968964228eaa39191bbb5508fa59270
Leonard Colin <leooo.colin@gmail.com>
2021-05-12 22:57:38 +0200

    Day 2 - Part one

--- day2/puzzle.py
+++ day2/puzzle.py
@@ -1,5 +1,24 @@
+from os import replace
+
+
 with open("/Users/leonardcolin/code/AdventOfCode/day2/password_list.txt") as file:
     data = [line for line in file]
 
-for password in data:
-    print(password.split()[0])
+password_lst = []
+for policy in data:
+    num = policy.split()[0]
+    num = [num for num in num.split("-", 1)]
+
+    min = int(num[0])
+    max = int(num[1])
+
+    letter = policy.split()[1].strip(":")
+    password = policy.split()[2]
+    
+    if min <= password.count(letter) <= max:
+        password_lst.append(password)
+    
+print(len(password_lst))
+
+
+    
